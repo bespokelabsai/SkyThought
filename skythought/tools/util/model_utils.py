9d62a767a508c99451e3dcd4f5c5bab612beaeff
ryanmarten <ryanmarten2000@gmail.com>
2025-01-21 14:13:22 -0800

    chkpt200

--- skythought/tools/util/model_utils.py
+++ skythought/tools/util/model_utils.py
@@ -86,6 +86,25 @@ Now, begin with the [ASSESS] action for the following task:
         {final formatted, precise, and clear solution} \
         <|end_of_solution|> \
         Now, try to solve the following question through the above guidelines:",
+    "bespokelabs/Stratos-R1-checkpoint-200": "Your role as an assistant involves thoroughly exploring questions through a systematic long \
+        thinking process before providing the final precise and accurate solutions. This requires \
+        engaging in a comprehensive cycle of analysis, summarizing, exploration, reassessment, reflection, \
+        backtracing, and iteration to develop well-considered thinking process. \
+        Please structure your response into two main sections: Thought and Solution. \
+        In the Thought section, detail your reasoning process using the specified format: \
+        <|begin_of_thought|> {thought with steps separated with '\n\n'} \
+        <|end_of_thought|> \
+        Each step should include detailed considerations such as analisying questions, summarizing \
+        relevant findings, brainstorming new ideas, verifying the accuracy of the current steps, refining \
+        any errors, and revisiting previous steps. \
+        In the Solution section, based on various attempts, explorations, and reflections from the Thought \
+        section, systematically present the final solution that you deem correct. The solution should \
+        remain a logical, accurate, concise expression style and detail necessary step needed to reach the \
+        conclusion, formatted as follows: \
+        <|begin_of_solution|> \
+        {final formatted, precise, and clear solution} \
+        <|end_of_solution|> \
+        Now, try to solve the following question through the above guidelines:",
     "bespokelabs/Stratos-R1-Micro": "Your role as an assistant involves thoroughly exploring questions through a systematic long \
         thinking process before providing the final precise and accurate solutions. This requires \
         engaging in a comprehensive cycle of analysis, summarizing, exploration, reassessment, reflection, \
@@ -161,6 +180,7 @@ MODEL_TO_NAME = {
     "PRIME-RL/Eurus-2-7B-PRIME": "Eurus-2-7B-PRIME",
     "NovaSky-AI/Sky-T1-32B-Preview": "Sky-T1-32B-Preview",
     "bespokelabs/Stratos-R1-checkpoint-100": "Stratos-R1-checkpoint-100",
+    "bespokelabs/Stratos-R1-checkpoint-200": "Stratos-R1-checkpoint-200",
     "bespokelabs/Stratos-R1-Micro": "Stratos-R1-Micro",
     "bespokelabs/Stratos-R1-Micro-Qwen": "Stratos-R1-Micro-Qwen",
     "bespokelabs/Stratos-R1-Micro-Llama": "Stratos-R1-Micro-Llama",
