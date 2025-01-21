a6f088da6aabe364a7b092b630719db7ae173e7f
ryanmarten <ryanmarten2000@gmail.com>
2025-01-21 08:29:33 -0800

    add

--- skythought/tools/util/model_utils.py
+++ skythought/tools/util/model_utils.py
@@ -67,6 +67,25 @@ Now, begin with the [ASSESS] action for the following task:
         {final formatted, precise, and clear solution} \
         <|end_of_solution|> \
         Now, try to solve the following question through the above guidelines:",
+    "bespokelabs/Stratos-R1-checkpoint-100": "Your role as an assistant involves thoroughly exploring questions through a systematic long \
+        thinking process before providing the final precise and accurate solutions. This requires \
+        engaging in a comprehensive cycle of analysis, summarizing, exploration, reassessment, reflection, \
+        backtracing, and iteration to develop well-considered thinking process. \
+        Please structure your response into two main sections: Thought and Solution. \
+        In the Thought section, detail your reasoning process using the specified format: \
+        <|begin_of_thought|> {thought with steps separated with '\n\n'} \
+        <|end_of_thought|> \
+        Each step should include detailed considerations such as analisying questions, summarizing \
+        relevant findings, brainstorming new ideas, verifying the accuracy of the current steps, refining \
+        any errors, and revisiting previous steps. \
+        In the Solution section, based on various attempts, explorations, and reflections from the Thought \
+        section, systematically present the final solution that you deem correct. The solution should \
+        remain a logical, accurate, concise expression style and detail necessary step needed to reach the \
+        conclusion, formatted as follows: \
+        <|begin_of_solution|> \
+        {final formatted, precise, and clear solution} \
+        <|end_of_solution|> \
+        Now, try to solve the following question through the above guidelines:",
     "openai/o1-mini": "Question: {input}\nAnswer: ",
     "openai/o1-preview": "Question: {input}\nAnswer: ",
     "openai/gpt-4o-mini": "User: {input}\nPlease reason step by step, and put your final answer within \\boxed{{}}.\n\nAssistant:",
@@ -84,7 +103,7 @@ MODEL_TO_NAME = {
     "Qwen/Qwen2.5-Math-7B-Instruct": "Qwen2.5-Math-7B-Instruct",
     "PRIME-RL/Eurus-2-7B-PRIME": "Eurus-2-7B-PRIME",
     "NovaSky-AI/Sky-T1-32B-Preview": "Sky-T1-32B-Preview",
-    "bespokelabs/Stratos-R1-checkpoint-100": "NovaSky-AI/Sky-T1-32B-Preview",  # to give same eval and prompt
+    "bespokelabs/Stratos-R1-checkpoint-100": "Stratos-R1-checkpoint-100",  # to give same eval and prompt
     "openai/o1-mini": "o1-mini",
     "openai/o1-preview": "o1-preview",  
     "openai/gpt-4o-mini": "gpt-4o-mini",
